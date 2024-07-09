6ed833c9a1011ab13354892f78481d00bcd492a0
mzunohkaru <winkaisyu@gmail.com>
2024-07-09 21:01:46 +0900

    fix fetcher

--- apps/client/src/lib/fetch.ts
+++ apps/client/src/lib/fetch.ts
@@ -1,55 +1,84 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, {
+  type Method,
+  type AxiosRequestConfig,
+  type AxiosResponse,
+} from "axios";
 
-export const instance = axios.create({
-  baseURL: "https://api.example.com",
-});
-
-export const makeRequest = async <T>(
-  config: AxiosRequestConfig
-): Promise<T> => {
+const makeRequest = async <T>(config: AxiosRequestConfig): Promise<T> => {
   try {
-    const response = await axios({
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
+    const response: AxiosResponse<T> = await axios({
+      headers: { "Content-Type": "application/json" },
       ...config,
     });
     return response.data;
   } catch (error) {
-    console.log(error);
-    throw new Error(`${error}`);
+    console.error(error);
+    throw error;
   }
 };
 
-export const getFetcher = <T, U>(
+export async function fetcherStatus<T, U = never>(
+  url: string,
+  method: Method,
+  data: T,
+  params?: U,
+  config?: AxiosRequestConfig
+): Promise<number> {
+  try {
+    const requestConfig = {
+      method: method,
+      data: { ...data },
+      params,
+      paramsSerializer: { indexes: null },
+      url,
+      ...config,
+    };
+    const response: AxiosResponse = await axios({
+      headers: { "Content-Type": "application/json" },
+      ...requestConfig,
+    });
+    return response.status;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function getFetcher<T, U = never>(
   url: string,
   params?: U,
   config?: AxiosRequestConfig
-): Promise<T> => {
-  return makeRequest<T>({ ...{ method: "GET", url, params, ...config } });
-};
+): Promise<T> {
+  return await makeRequest<T>({
+    ...{
+      method: "GET",
+      params,
+      paramsSerializer: { indexes: null },
+      url,
+      ...config,
+    },
+  });
+}
 
-export const postFetcher = <T, U>(
+export async function postFetcher<T, U>(
   url: string,
   data: U,
-  config: AxiosRequestConfig
-): Promise<T> => {
-  return makeRequest<T>({ ...{ method: "POST", url, data, ...config } });
-};
+  config?: AxiosRequestConfig
+): Promise<T> {
+  return await makeRequest<T>({ ...{ method: "POST", data, url, ...config } });
+}
 
-export const putFetcher = <T, U>(
+export async function putFetcher<T, U = never>(
   url: string,
-  data: U,
-  config: AxiosRequestConfig
-): Promise<T> => {
-  return makeRequest<T>({ ...{ method: "PUT", url, data, ...config } });
-};
+  data?: U,
+  config?: AxiosRequestConfig
+): Promise<T> {
+  return await makeRequest<T>({ ...{ method: "PUT", data, url, ...config } });
+}
 
-export const deleteFetcher = <T, U>(
+export async function deleteFetcher<T>(
   url: string,
-  data: U,
-  config: AxiosRequestConfig
-): Promise<T> => {
-  return makeRequest<T>({ ...{ method: "DELETE", url, data, ...config } });
-};
+  config?: AxiosRequestConfig
+): Promise<T> {
+  return await makeRequest<T>({ method: "DELETE", url, ...config });
+}
